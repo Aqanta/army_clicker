6929113d3beb1d87dbfb4e9d7907defd4ed0f2c4
OrangeJedi <isaac@2b4joy.com>
2017-02-14 16:58:43 -0700

    Added game start

--- js.js
+++ js.js
@@ -3,9 +3,17 @@
  */
 //Variable that holds the game data
 var game = {"land" : 100};
+//function to start the game
+function start(){
+    showGame();
+}
 //function to update the game
 function tick(){
 
 }
 //calls the tick function every second
 setInterval(tick(),1000);
+//function that control the screen
+function showGame(){
+    $('#screen').text("You started the game. Good Job!");
+}
