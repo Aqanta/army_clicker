85cf8b261db34fb2f537668040a1efa8415dd07a
OrangeJedi <OrangeJedi@users.noreply.github.com>
2017-02-16 14:08:05 -0700

    Added logic to check techs
    need to check code before running, not made with IDE.

--- Code Segments/tech tree/js.js
+++ Code Segments/tech tree/js.js
@@ -9,5 +9,26 @@ var techTree = [
   {"name": "Chisel","class" : "Tools", "classNum" : 1.2, "preRec" : [], "grants" : {}, "have" : false, "tier" : 1, "id" : 4},
   {"name": "Axe","class" : "Tools", "classNum" : 1.1, "preRec" : [], "grants" : {}, "have" : false, "tier" : 1, "id" : 5},
   {"name": "Woodcutter","class" : "Professions", "classNum" : 1, "preRec" : [], "grants" : {}, "have" : false, "tier" : 1, "id" : 6},
-  {"name": "Hoe","class" : "Farming Tools", "classNum" : 1, "preRec" : [0], "grants" : {}, "have" : false, "tier" : 2, "id" : 0},
+  {"name": "Hoe","class" : "Farming Tools", "classNum" : 1, "preRec" : [0], "grants" : {}, "have" : false, "tier" : 2, "id" : 7},
                ];
+function checkTechTree(){
+  var valid = [];
+  for(var i = 0;i < techTree.length;i++){
+    if(techTree[i].preRec === {} && !techTree[i].have){
+      valid.push(i);
+    }else if(valid.contains(techTree[i].preRec)  && !techTree[i].have){
+      valid.push(i);
+    }
+  }
+  return valid;
+};
+function updateTechTree(){
+  var techs = checkTechTree;
+  var list = "<ul>";
+  for(var i = 0;i < list.length;i++){
+    list = list + "<li>" + techTree[i].name + "</li>";
+  }
+  list = list + "</ul>"
+  $("#tree").text(list);
+}
+updateTechTree();
