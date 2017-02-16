16acc5938d8c8a6dac8acbad097b09f72b1299c5
OrangeJedi <OrangeJedi@users.noreply.github.com>
2017-02-16 13:22:56 -0700

    Added more techs

--- Code Segments/tech tree/js.js
+++ Code Segments/tech tree/js.js
@@ -1,4 +1,13 @@
 /**
  * Created by OrangeJedi on 2/15/2017.
  */
-var techTree = [{"name": "Farming","class" : "Food", "classNum" : 1, "preRec" : {}, "grants" : {}, "have" : false}];
+var techTree = [
+  {"name": "Farming","class" : "Food", "classNum" : 1, "preRec" : [], "grants" : {}, "have" : false, "tier" : 1, "id" : 0},
+  {"name": "Herbs","class" : "Health Care", "classNum" : 1, "preRec" : [], "grants" : {}, "have" : false, "tier" : 1, "id" : 1},
+  {"name": "Trails","class" : "Infrastructure", "classNum" : 1, "preRec" : [], "grants" : {}, "have" : false, "tier" : 1, "id" : 2},
+  {"name": "Tent","class" : "Housing", "classNum" : 1, "preRec" : [], "grants" : {}, "have" : false, "tier" : 1, "id" : 3},
+  {"name": "Chisel","class" : "Tools", "classNum" : 1.2, "preRec" : [], "grants" : {}, "have" : false, "tier" : 1, "id" : 4},
+  {"name": "Axe","class" : "Tools", "classNum" : 1.1, "preRec" : [], "grants" : {}, "have" : false, "tier" : 1, "id" : 5},
+  {"name": "Woodcutter","class" : "Professions", "classNum" : 1, "preRec" : [], "grants" : {}, "have" : false, "tier" : 1, "id" : 6},
+  {"name": "Hoe","class" : "Farming Tools", "classNum" : 1, "preRec" : [0], "grants" : {}, "have" : false, "tier" : 2, "id" : 0},
+               ];
