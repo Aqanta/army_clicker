2d3f8285ceddc763cd2f0235b9056369d6961748
OrangeJedi <OrangeJedi@users.noreply.github.com>
2017-02-17 08:26:04 -0700

    Added support for "or" in the tech tree
    use array in preRec field. untested code. The code should allow for more than one "or" statement with multiple techs in the "or" statement allowed. "or" can be accompanied by "and" as well.

--- Code Segments/tech tree/js.js
+++ Code Segments/tech tree/js.js
@@ -9,7 +9,8 @@ var techTree = [
   {"name": "Chisel","class" : "Tools", "classNum" : 1.2, "preRec" : [], "grants" : {}, "have" : false, "tier" : 1, "id" : 4},
   {"name": "Axe","class" : "Tools", "classNum" : 1.1, "preRec" : [], "grants" : {}, "have" : false, "tier" : 1, "id" : 5},
   {"name": "Woodcutter","class" : "Professions", "classNum" : 1, "preRec" : [], "grants" : {}, "have" : false, "tier" : 1, "id" : 6},
-  {"name": "Hoe","class" : "Farming Tools", "classNum" : 1, "preRec" : [0], "grants" : {}, "have" : false, "tier" : 2, "id" : 7}
+  {"name": "Hoe","class" : "Farming Tools", "classNum" : 1, "preRec" : [0], "grants" : {}, "have" : false, "tier" : 2, "id" : 7},
+  {"name": "Millitary","class" : "Military", "classNum" : 1, "preRec" : [[0,2]], "grants" : {}, "have" : false, "tier" : 2, "id" : 8}
                ];
 function checkTechTree(){
   var valid = [];
@@ -18,12 +19,24 @@ function checkTechTree(){
       valid.push(i);
     }else if(!techTree[i].have){
         var c = 0;
-        for(var j = 0; j < techTree[i].preRec.length;j++){
+        var d = techTree[i].preRec.length;
+        for(var j = 0; j < d;j++){
+          if(!techTree[i].preRec[j].isArray()){
             if(techTree[techTree[i].preRec[j]].have){
                 c++;
             }
+          }else{
+            d = d + 1;
+            var e = 0;
+            for(var k = 0; k < techTree[i].preRec[j].length;k++){
+              if(techTree[techTree[i].preRec[j][k]].have && e == 0){
+                c++;
+                e++;
+              }
+            }
+          }
         }
-        if(c == techTree[i].preRec.length){
+        if(c == d){
             valid.push(i);
         }
     }
