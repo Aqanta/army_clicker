d2c7564ba3977b42b631942f6b414664a6276953
OrangeJedi <isaac@2b4joy.com>
2017-02-21 18:21:51 -0700

    worked on death

--- Code Segments/population simulator/popSim.js
+++ Code Segments/population simulator/popSim.js
@@ -9,27 +9,42 @@ var game = {
         "lables" :["105+","100-104","95-99","80-84","75-79","70-74","65-69","60-64","55-59","50-54","45-49","40-44","35-39","30-34","25-29","20-24","15-19","10-14","5-9","0-4"]
     },
     "demo" :{
-        "cbr" : 40
+        "cbr" : 40,
+        "cdr" : 40
     }
 };
 function start() {
-    game.pop.data.push(new PopSeg(-1,100));
+    //game.pop.data.push(new PopSeg(-1,100));
+    var startingPop = 100;
+    for(var i = 0;i < startingPop/4;i++){
+        game.pop.data.push(new PopSeg(i, ((startingPop/5)/5)));
+    }
     tick();
 }
 function tick(){
     var popSize = 0;
-    var pmdM = [0,0,0,0,0,0,0,0,0,0,0,0];
-    var pmdF = [0,0,0,0,0,0,0,0,0,0,0,0];
+    var pmdM = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    var pmdF = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
     for(var i = 0;i < game.pop.data.length;i++){
+        //ages population
         game.pop.data[i].age++;
-        popSize += game.pop.data[i].size;
-        if(game.pop.data[i].age < 55) {
+        //build population pmd data
+        if(game.pop.data[i].age < 105) {
             pmdM[Math.floor(game.pop.data[i].age / 5)] -= game.pop.data[i].male;
             pmdF[Math.floor(game.pop.data[i].age / 5)] += game.pop.data[i].female;
+        }else{
+            pmdM[20] -= game.pop.data[i].male;
+            pmdF[20] += game.pop.data[i].female;
         }
+        //kills off population
+        /*game.pop.data[i].male -= Math.ceil(game.pop.data[i].size * ((game.demo.cdr / 1000) * (1 + (game.pop.data[i].age / 100))));
+        game.pop.data[i].female -= Math.ceil(game.pop.data[i].size * ((game.demo.cdr / 1000) * (1 + (game.pop.data[i].age / 100))));
+        game.pop.data[i].size = game.pop.data[i].male + game.pop.data[i].female;*/
+        //totals up population
+        popSize += game.pop.data[i].size;
     }
 
-    for(i = 0;i < pmdM;i++){
+    for(i = 0;i < pmdM.length;i++){
         pmdM[i] = pmdM[i] / popSize;
         pmdF[i] = pmdF[i] / popSize;
     }
@@ -40,11 +55,11 @@ function tick(){
     game.pop.pmd.male = pmdM.reverse();
     game.pop.pmd.female = pmdF.reverse();
     printOut();
-    setTimeout(tick,1000);
+    setTimeout(tick,500);
 }
 function printOut(){
     $('#output').text(game.pop.data[0].age + "  " + game.pop.size);
-    var max = 60;
+    var max = .15;
     var mPMD = new Chart(ctxM, {
         type: 'horizontalBar',
         data: {
@@ -131,9 +146,17 @@ function printOut(){
 function PopSeg(age,size){
     this.age = age;
     this.size = size;
-    this.male = Math.floor(size * (Math.floor((Math.random() * 10) + 45) / 100));
+    this.male = Math.floor(size * (Math.floor((Math.random() * 3) + 49) / 100));
     this.female = size - this.male;
     this.edu = 0;
+}
+function adder(){
+    var total = 0;
+    for(var i = 0;i < game.pop.pmd.female.length;i++){
+        total += game.pop.pmd.female[i];
+        total += game.pop.pmd.male[i];
+    }
+    return total;
 }
 var ctxM = document.getElementById("malePMD");
 var ctxF = document.getElementById("femalePMD");
